8b445296aad12f31ab85895b5b3e0e694dd41206
Evelyne Maquelin <maqueline@catlin.edu>
2017-04-20 20:31:49 -0400

    final favicon

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 	echo date('Y-m-d H:i:s');
 	readfile("pages/header.html");
 	readfile("pages/home.html");
+	if ($_REQUEST) {
+		contents = $_REQUEST["text_box"];
+    	file_put_contents("t.txt", contents);
+	}
+	readfile("t.txt");
 	readfile("pages/footer.html");
 ?>
