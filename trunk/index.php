2e4eabe5e5a166e6e93e1f4b8cfe218efff027b6
Evelyne Maquelin <maqueline@catlin.edu>
2017-04-20 20:50:20 -0400

    don't look

---
+++ trunk/index.php
@@ -0,0 +1,9 @@
+<?php
+	$myServer = "golem.csse.rose-hulman.edu";
+	$myUser = "maquelea";
+	$myPass = "bubbles2016GO!";
+	$myDB = "gdm-final";
+
+	$dbhandle = mssql_connect($myServer, $myUser, $myPass)
+	  or die("Couldn't connect to SQL Server on $myServer"); 
+?>
